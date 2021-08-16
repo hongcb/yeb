7b09fe6456b8efec521411ff7bd239e232b1a013
hongcb <891539723@qq.com>
2021-08-16 19:20:54 +0800

    登陆功能完善

--- yeb-server/src/main/java/com/xxxx/yebserver/service/impl/AdminServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/yebserver/service/impl/AdminServiceImpl.java
@@ -1,13 +1,23 @@
 package com.xxxx.yebserver.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xxxx.yebserver.config.JwtTokenUtil;
 import com.xxxx.yebserver.mapper.AdminMapper;
 import com.xxxx.yebserver.pojo.Admin;
 import com.xxxx.yebserver.pojo.RespBean;
 import com.xxxx.yebserver.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -20,8 +30,33 @@ import javax.servlet.http.HttpServletRequest;
 @Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements IAdminService {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+
     @Override
     public RespBean login(String username, String password, HttpServletRequest request) {
-        return null;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if(null == userDetails  || passwordEncoder.matches(password,userDetails.getPassword())){
+            return RespBean.error("用户名或者密码不正确");
+        }
+        if(!userDetails.isEnabled()){
+            return RespBean.error("账号被禁用，请联系管理员");
+        }
+        //更新security登录用户对象
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        //生成token
+        String token = jwtTokenUtil.generateToken(userDetails);
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        tokenMap.put("tokenHead", tokenHead);
+        return RespBean.success("登录成功", tokenMap);
     }
 }

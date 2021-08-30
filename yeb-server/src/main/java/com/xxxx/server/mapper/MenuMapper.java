26e9410e8ec36a02b3c8422a060be167edf99b1b
hongcb <891539723@qq.com>
2021-08-30 20:39:30 +0800

    一级菜单功能开发

--- yeb-server/src/main/java/com/xxxx/server/mapper/MenuMapper.java
+++ yeb-server/src/main/java/com/xxxx/server/mapper/MenuMapper.java
@@ -4,6 +4,8 @@ package com.xxxx.server.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xxxx.server.pojo.Menu;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -13,5 +15,10 @@ import com.xxxx.server.pojo.Menu;
  * @since 2021-08-11
  */
 public interface MenuMapper extends BaseMapper<Menu> {
-
+    /**
+     * 通过用户id获取菜单列表
+     * @param id
+     * @return
+     */
+    List<Menu> getMenusByAdminId(Integer id);
 }
--- yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
@@ -3,9 +3,15 @@ package com.xxxx.server.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxxx.server.mapper.MenuMapper;
+import com.xxxx.server.pojo.Admin;
 import com.xxxx.server.pojo.Menu;
 import com.xxxx.server.service.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +23,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IMenuService {
+    @Resource
+    private MenuMapper menuMapper;
 
+    /**
+     * 通过用户id获取菜单列表
+     * @return
+     */
+    @Override
+    public List<Menu> getMenusByAdminId() {
+        return menuMapper.getMenusByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+    }
 }
